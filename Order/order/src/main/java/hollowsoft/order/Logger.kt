a44e4a762e40b3517bbf240ca60603c3289eb862
Igor Morais <igor.morais@hollowsoft.com>
2020-01-23 17:27:08 -0300

    Minor improvement

--- Order/order/src/main/java/hollowsoft/order/Logger.kt
+++ Order/order/src/main/java/hollowsoft/order/Logger.kt
@@ -8,7 +8,9 @@ import android.util.Log
 class Logger {
     companion object {
         fun info(tag: String, message: String) = Log.i(tag, message)
+
         fun warn(tag: String, message: String) = Log.w(tag, message)
+
         fun error(tag: String, message: String) = Log.e(tag, message)
     }
 }
