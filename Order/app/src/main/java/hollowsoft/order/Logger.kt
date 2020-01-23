9ff941148a93e584bd69ddca11179ad826d9c75a
Igor Morais <igor.morais@hollowsoft.com>
2020-01-22 21:24:06 -0300

    Implement logger

---
+++ Order/app/src/main/java/hollowsoft/order/Logger.kt
@@ -0,0 +1,14 @@
+package hollowsoft.order
+
+import android.util.Log
+
+/**
+ * @author Igor Morais
+ */
+class Logger {
+    companion object {
+        fun info(tag: String, message: String) = Log.i(tag, message)
+        fun warn(tag: String, message: String) = Log.w(tag, message)
+        fun error(tag: String, message: String) = Log.e(tag, message)
+    }
+}
